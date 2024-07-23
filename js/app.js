b531ea18a46322817f5e795a9801795848c1c5e0
Vadim Azimov <vadim@Vadims-MacBook-Air.local>
2024-07-23 16:45:23 -0400

    Updated script.js

---
+++ js/app.js
@@ -0,0 +1,22 @@
+// 1) Define any variables used to track the state of the game:
+//    The players choice
+//    Current round data
+//          Chioices
+//          Correct answer
+//          Question
+//    The match result - win/lose
+//    A result message - display who won that round
+
+// 2) Define the required constants:
+//    There are only 3 choices a user can make, create buttons
+//    We'll need a reference to a DOM element to display masseges for both questions and answers
+
+// 3) Handle a player clicking a choice button
+
+// 4) Handle generating random selections for the computer player
+
+// 5) Compare the player choice to the computer choice, and check for a winner
+
+// 6) Render a win/lose message to the player 
+//    Include both player and computer selections in the message
+//    Clearly indicate who won
